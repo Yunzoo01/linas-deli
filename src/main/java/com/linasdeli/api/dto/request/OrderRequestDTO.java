3331edf23541a7bfacf0151bb73e1e6bd83bf498
ljsunny <js961223@naver.com>
2025-03-12 23:28:36 -0700

    #10 - fix Order Domain

--- src/main/java/com/linasdeli/api/dto/request/OrderRequestDTO.java
+++ src/main/java/com/linasdeli/api/dto/request/OrderRequestDTO.java
@@ -1,5 +1,6 @@
 package com.linasdeli.api.dto.request;
 
+import com.linasdeli.api.domain.Platter;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,11 @@ import lombok.Setter;
 public class OrderRequestDTO {
     private String customerName;
     private String email;
+    private String platter;
 
-    public OrderRequestDTO(String customerName, String email) {
+    public OrderRequestDTO(String customerName, String email, String platter) {
         this.customerName = customerName;
         this.email = email;
+        this.platter = platter;
     }
 }
