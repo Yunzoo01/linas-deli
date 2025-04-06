c9a2933c6e7258358a8c152da191c27c012174c8
ljsunny <js961223@naver.com>
2025-04-05 18:20:39 -0700

    #10 - update Order email func

--- src/main/java/com/linasdeli/api/dto/request/OrderRequestDTO.java
+++ src/main/java/com/linasdeli/api/dto/request/OrderRequestDTO.java
@@ -24,7 +24,6 @@ public class OrderRequestDTO {
 
     private String phone;
 
-    @NotBlank(message = "Status is required")
     private String status;
 
     @NotNull(message = "Date is required")
